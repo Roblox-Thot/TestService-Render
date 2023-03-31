267c18afad4195e8494c0cd7159b068a5667e3ad
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2023-03-31 15:09:06 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import json, base64
 
+dumpJson = True # do you want the json to be dumped at the end?
+
 app = Flask(__name__)
 
 @app.route('/api/v1/submit_test', methods = ['POST'])
@@ -8,10 +10,18 @@ def receive():
     try:
         data = request.data
         data = json.loads(data)
-        with open("Base64.txt", "w")  as f: # Puts the Base64 of the image in Base64.txt
+        name = data['name']
+        print(name + " Base64.txt")
+        with open(name + " Base64.txt", "w") as f: # Puts the Base64 of the image in (ObjectName) Base64.txt
             f.write("data:image/png;base64,"+data['image'])
-        with open("Image.png", "wb")  as f: # Outputs the image to Image.png
+
+        with open(name + " Image.png", "wb") as f: # Outputs the image to (ObjectName) Image.png
             f.write(base64.b64decode(data['image']))
+
+        if dumpJson:
+            with open(name + " data.json", "w")  as f:
+                f.write(json.dump(data,f,indent=4))
+
         return "passed" # Tells Studio it passed
     except:
         return "failed" # Used to tell Studio if it failed (tell me in a issue)
@@ -21,4 +31,4 @@ def bye(): # Only needed to prevent errors
     return ""
 
 if __name__ == '__main__':
-    app.run(port=8001)
+    app.run(port=8001,debug=True)
