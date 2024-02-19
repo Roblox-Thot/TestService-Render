18b6803fb775deb2c3311b39f690e7dc74761258
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-02-18 19:41:07 -0800

    :)

--- index.py
+++ index.py
@@ -25,32 +25,32 @@ def receive():
         name = data['name']
 
         if save_image:
-            with open(f"{name} Image.png", "wb") as f: # Outputs the image to (ObjectName) Image.png
+            with open(f'{name} Image.png', 'wb') as f: # Outputs the image to (ObjectName) Image.png
                 f.write(base64.b64decode(data['image']))
 
         if save_base64:
-            with open(f"{name} Base64.txt", "w") as f: # Puts the Base64 of the image in (ObjectName) Base64.txt
-                f.write("data:image/png;base64," + data['image'])
+            with open(f'{name} Base64.txt', 'w') as f: # Puts the Base64 of the image in (ObjectName) Base64.txt
+                f.write('data:image/png;base64,' + data['image'])
 
         if dump_json:
-            with open(f"{name} data.json", "w") as f:
+            with open(f'{name} data.json', 'w') as f:
                 json.dump(data, f, indent=4)
 
-        return "passed" # Tells Studio it passed
+        return 'passed' # Tells Studio it passed
     except:
-        return "failed" # Used to tell Studio if it failed (tell me in a issue)
+        return 'failed' # Used to tell Studio if it failed (tell me in a issue)
 
 @app.route('/api/v1/alltestfinised', methods = ['POST'])
 def bye(): # Only needed to prevent errors
-    return ""
+    return ''
 
 @app.route('/<path:p>', methods=['GET', 'POST'])
 def all_routes(p): # Catch all just incase
-    return ""
+    return ''
 
 @app.route('/', methods=['GET', 'POST'])
 def home(): # Prob unneeded but just incase they add a check or something
-    return "OK"
+    return 'OK'
 
 if __name__ == '__main__':
     app.run(port=8001, debug=False)
